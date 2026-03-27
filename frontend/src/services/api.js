449a453c89c0c9409971ee93da5a510a69ac6c88
sukrut07 <sukrut.dusane@gmail.com>
2026-03-27 19:10:27 +0530

    site changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:5005/api/auth",
+    baseURL: "http://localhost:5005/api",
     withCredentials: false,
     headers: {
         'Content-Type': 'application/json',
