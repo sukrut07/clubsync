c878294c34ee55bbbd70550523ba048802534105
sukrut07 <sukrut.dusane@gmail.com>
2026-03-27 19:01:42 +0530

    site changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "https://clubsync-8js9.onrender.com/api",
+    baseURL: "https://clubsync-4qua.vercel.app/",
     withCredentials: false,
     headers: {
         'Content-Type': 'application/json',
